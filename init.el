f07b7affd4e0c5f0574cb72bd982b3c4ab6d7545
caasi Huang <caasi@users.noreply.github.com>
2016-02-19 02:54:32 +0800

    Hello from the other side.

---
+++ init.el
@@ -0,0 +1,4 @@
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "https://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
